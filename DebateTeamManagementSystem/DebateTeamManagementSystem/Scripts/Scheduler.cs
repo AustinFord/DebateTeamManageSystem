cc61e44c71bd64acdfcf47caf9befe92a7123ac4
KBrannon <kbranno7@students.kennesaw.edu>
2016-10-18 15:06:14 -0400

    Created Utility class Created Scheduler class
    Utility class currently
    public struct TimeSlot
    public struct Vec2
    public static T[] ExtendArray<T> (T[] array, int extensions)
    public static T[] RemoveAt<T> (T[] array, int index)
    
    Scheduler class currently
    various variables
    public string CreateSchedule ()
    Getters/Setters

---
+++ DebateTeamManagementSystem/DebateTeamManagementSystem/Scripts/Scheduler.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DebateTeamManagementSystem.Scripts
+{
+    /// <summary>
+    /// Module used to sort out a schedule for the debates.
+    /// </summary>
+    public class Scheduler
+    {
+        // Only need Month, Day, Year.  Time will be sorted using the timeSlot variable.
+        // Time is set to midnight with the 'set' for these.
+        DateTime startDate, endDate;
+
+        // Ex: float[8.f, 20.5f] would be two time slots per day, 8:30am and 8:30pm
+        float[] hourSlots;
+
+        // Special struct that holds the DateTime of the debate, both team indices, and both team scores.
+        Util.TimeSlot[] timeSlots;
+        
+        // Needs to contain teamNames or int IDs in case a team drops out.
+        // We will use a special case value like -1 to indicate a team that
+        // is no longer in the running.  This will maintain data integrity as
+        // the team IDs will all stay the same throughout the season.
+        string[] teamList;
+
+        // list of team indices where even numbers are team1 and odd numbers are team2 of the fight.
+        // Should only be used internally in this module.
+        // External references of the pairings should use the timeSlots array.
+        Util.Vec2[] fightPairings;
+
+        /// <summary>
+        /// Constructor, not currently used.
+        /// </summary>
+        public Scheduler () { }
+
+        /// <summary>
+        /// Uses a DateTime startDate, DateTime endDate, float[] timeSlots, and string[] teamList set as updated from the editable display to
+        /// sort out if a proper shcedule with a minimum amount of extra slots is available.  Will return a string describing whether more
+        /// time slots or more weeks should be added to the season to accomodate the debates and a suitable amount for unforseen schedule changes.
+        /// </summary>
+        /// <returns>string message</returns>
+        public string CreateSchedule ()
+        {
+            // This assumes that the SetStartDate method was used to assign the startDate (startDate is a Saturday)
+            int possibleDays = (int)((endDate - startDate).TotalDays / 7);
+
+            // Create TimeSlot[] with slots for each hourSlot of each day.
+            timeSlots = new Util.TimeSlot[possibleDays * hourSlots.Length];
+            for (int i = 0; i < timeSlots.Length; i++)
+            {
+                timeSlots[i].timeSlot = startDate.AddDays((int)(i / hourSlots.Length));
+            }
+
+            // Make a tempArray of all team pairings
+            Util.Vec2[] tempPairings = new Util.Vec2[0];
+            // Assign pairings
+            for (int i = 0; i < teamList.Length-1; i++)
+            {
+                for (int j = i+1; j < teamList.Length; j++)
+                {
+                    // Make a new slot for this pair
+                    tempPairings = Util.ExtendArray<Util.Vec2>(tempPairings, 1);
+                    // Assign the pair
+                    tempPairings[tempPairings.Length - 1] = new Util.Vec2(i, j);
+                }
+            }
+            
+            // Sort the tempArray into the fightPairings array using our algorithm.
+            fightPairings = new Util.Vec2[tempPairings.Length];
+            int fightIndex = 0;
+            for (int i = 0; i < teamList.Length-1; i++)
+            {
+                int index = 0;
+                for (int j = teamList.Length-1; index < tempPairings.Length; j--)
+                {
+                    fightPairings[fightIndex] = tempPairings[index];
+                    fightIndex++;
+                    index += j;
+                }
+                tempPairings = Util.RemoveAt<Util.Vec2>(tempPairings, 0);
+            }
+
+            return "This schedule is acceptable";
+        }
+
+        // All sets can be done as they are changed in the Editable Display since it's only once at the time of change.
+        #region Gets and Sets
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set
+            {
+                // Get the next Saturday from the incoming date, if the date is already Saturday, make no change.
+                int firstSaturday = ((int)DayOfWeek.Saturday - (int)value.DayOfWeek + 7) % 7;
+                startDate = value.AddDays(firstSaturday).Date;
+            }
+        }
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set
+            {
+                // Get the next Saturday from the incoming date, if the date is already Saturday, make no change.
+                int firstSaturday = ((int)DayOfWeek.Saturday - (int)value.DayOfWeek + 7) % 7;
+                endDate = value.AddDays(firstSaturday).Date;
+            }
+        }
+        
+        /// <summary>
+        /// Takes in an array of floats describing the hourSlots that the debates will take place.
+        /// </summary>
+        /// <param name="slots">0 to 23.9~ describing each hour and percent of the hour (0.5f = 30 minutes)</param>
+        public float[] HourSlots
+        {
+            get { return hourSlots; }
+            set { hourSlots = value; }
+        }
+        
+        public string[] TeamList
+        {
+            get { return teamList; }
+            set { teamList = value; }
+        }
+
+        public Util.Vec2[] Pairings
+        {
+            get { return fightPairings; }
+            // Setting fightPairings might become obsolete.  Depends on if we want to allow manual overrides and how they are handled.
+            set { fightPairings = value; }
+        }
+        #endregion
+    }
+}
---
+++ DebateTeamManagementSystem/DebateTeamManagementSystem/Scripts/Util.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DebateTeamManagementSystem.Scripts
+{
+    /// <summary>
+    /// Utility class to be used for various functions and custom structs
+    /// </summary>
+    public static class Util
+    {
+        // Struct to hold all debate schedules, the teams involved, and the scores of each team
+        // Might eventually hold the referee assigned to the debate
+        public struct TimeSlot
+        {
+            public DateTime timeSlot;
+            public int team1Index, team1Score, team2Index, team2Score;
+        }
+
+        // Struct to hold pairs of floats
+        public struct Vec2
+        {
+            public float x, y;
+
+            public Vec2(float inX = 0, float inY = 0)
+            {
+                x = inX;
+                y = inY;
+            }
+        }
+
+        /// <summary>
+        /// Takes an array of any type and the number of elements to add or subtract from the total, then does.
+        /// If "extensions" is negative, it will cut off that many elements from the end.
+        /// </summary>
+        /// <typeparam name="T">Array Type</typeparam>
+        /// <param name="array">The array to change the size of</param>
+        /// <param name="extensions">number of elements to add or subtract.</param>
+        public static T[] ExtendArray<T> (T[] array, int extensions)
+        {
+            // If extensions would subtract more slots than the array has, just return a zero length array.
+            if (-extensions > array.Length)
+                return new T[0];
+
+            // Create a temp array with a length of the inbound array adjusted by the extentions argument.
+            T[] tempArray = new T[array.Length + extensions];
+            // Start copying values up to the length of the NEW array.  This prevents leaving the bounds of a shortened array.
+            for (int i = 0; i < array.Length + extensions; i++)
+            {
+                // If we run out of values, stop trying to copy them over
+                if (i <= array.Length)
+                    break;
+
+                tempArray[i] = array[i];
+            }
+            return tempArray;
+        }
+
+        /// <summary>
+        /// Removes slot of array at 'index'
+        /// </summary>
+        /// <typeparam name="T">Type of Array</typeparam>
+        /// <param name="array">Array to remove from</param>
+        /// <param name="index">Index to remove at</param>
+        /// <returns>Modified array</returns>
+        public static T[] RemoveAt<T> (T[] array, int index)
+        {
+            // Worker boolean to identify the removed index and modify the copy algorithm
+            bool removed = false;
+            // New Array
+            T[] tempArray = new T[array.Length-1];
+            // Copy over the values from the old array, skipping the value at index
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                // Once index is found, set Removed to true and skip that value
+                if (i == index)
+                    removed = true;
+                // Shorthand if statement that will shift the indicies by 1 once the removed index is found.
+                tempArray[i] = array[i + (removed ? 1 : 0)];
+            }
+            return tempArray;
+        }
+    }
+}
